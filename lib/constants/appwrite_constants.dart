aae5510e6f8c7fcbb37a7c30fd1ddae8498b9d54
Sheersh <sheershbhatnagar2@zohomail.in>
2023-09-10 01:35:48 +0530

    Initial Commit & Adding Assets

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,8 @@
+
+class AppWriteConstants {
+
+  static const String projectID = '64fc84f7edab5c1988b1';
+  static const String databaseID = '64fc933625f53318f3a9';
+  static const String endPoint = 'https://cloud.appwrite.io/v1';
+
+}
